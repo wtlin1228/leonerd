a207f5258ee0f005c60f24f693e8804c23ef83e3
wtlin1228 <wtlin1228@gmail.com>
2023-04-29 00:10:36 +0800

    fix: post CSS should all start from .post to narrow the impact

--- packages/blog/src/routes/posts/layout.tsx
+++ packages/blog/src/routes/posts/layout.tsx
@@ -1,67 +1,21 @@
-import { component$, Slot, useContext, useStyles$ } from '@builder.io/qwik';
-import { useContent, useLocation } from '@builder.io/qwik-city';
-// import { ContentNav } from '../../components/content-nav/content-nav';
-// import { Footer } from '../../components/footer/footer';
-// import { Header } from '../../components/header/header';
-// import { OnThisPage } from '../../components/on-this-page/on-this-page';
-// import { createBreadcrumbs, SideBar } from '../../components/sidebar/sidebar';
-// import { GlobalStore } from '../../context';
+import { component$, Slot, useStyles$ } from '@builder.io/qwik';
+
 import styles from './posts.css?inline';
-// import Contributors from '../../components/contributors';
 
 export default component$(() => {
-  // const loc = useLocation();
-  // const noRightMenu = ['/docs/'].includes(loc.url.pathname);
+  // Because we are using rehype with MDX, we need to use
+  // the `useStyles$` hook to avoid the styles being
+  // postfixed with some random string.
   useStyles$(styles);
-  // const { menu } = useContent();
-  // const globalStore = useContext(GlobalStore);
-  // const { url } = useLocation();
-  // const breadcrumbs = createBreadcrumbs(menu, url.pathname);
 
   return (
     <div class="post">
-      {/* <Header />
-      <nav class="breadcrumbs">
-        <button
-          onClick$={() => (globalStore.sideMenuOpen = !globalStore.sideMenuOpen)}
-          type="button"
-          title="Toggle left menu"
-          aria-label="Toggle left menu"
-        >
-          <span class="sr-only">Navigation</span>
-          <svg width="24" height="24">
-            <path
-              d="M5 6h14M5 12h14M5 18h14"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-            />
-          </svg>
-        </button>
-        {breadcrumbs.length > 0 ? (
-          <ol>
-            {breadcrumbs.map((b) => (
-              <li>{b.text}</li>
-            ))}
-          </ol>
-        ) : null}
-      </nav>
-      <SideBar /> */}
-      <main
-      // class={{
-      //   'no-right-menu': noRightMenu,
-      // }}
-      >
+      <main>
         <div class="post-container">
           <article>
             <Slot />
-            {/* <Contributors /> */}
           </article>
-          {/* <ContentNav /> */}
-          {/* <Footer /> */}
         </div>
-        {/* <OnThisPage /> */}
       </main>
     </div>
   );
