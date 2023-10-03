755efbf595b9a5471cc523331b8790b4677c1314
wtlin1228 <wtlin1228@gmail.com>
2023-10-04 02:03:29 +0800

    docs: add cover image for about page

--- packages/blog/src/routes/about/layout.tsx
+++ packages/blog/src/routes/about/layout.tsx
@@ -24,71 +24,71 @@ export default component$(() => {
 
 export const head: DocumentHead = {
   title: 'About Me | LeoNerd',
-  // links: [
-  //   {
-  //     rel: 'preload',
-  //     // @ts-expect-error fetchpriority is not supported by all browsers
-  //     fetchpriority: 'high',
-  //     as: 'image',
-  //     type: 'image/webp',
-  //     href: '/about/cover.webp',
-  //   },
-  // ],
-  // meta: [
-  //   {
-  //     name: 'description',
-  //     content:
-  //       "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
-  //   },
-  //   {
-  //     property: 'og:title',
-  //     content: 'About Me | LeoNerd',
-  //   },
-  //   {
-  //     property: 'og:description',
-  //     content:
-  //       "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
-  //   },
-  //   {
-  //     property: 'og:image',
-  //     content: import.meta.env.PUBLIC_BLOG_URL + '/about/cover.webp',
-  //   },
-  //   {
-  //     property: 'og:image:alt',
-  //     content: 'Image for about page',
-  //   },
-  //   {
-  //     property: 'og:image:width',
-  //     content: '1000',
-  //   },
-  //   {
-  //     name: 'og:image:height',
-  //     content: '600',
-  //   },
-  //   {
-  //     name: 'twitter:card',
-  //     content: 'summary_large_image',
-  //   },
-  //   {
-  //     name: 'twitter:site',
-  //     content: '@WeiTangLin1',
-  //   },
-  //   {
-  //     name: 'twitter:title',
-  //     content: 'About Me | LeoNerd',
-  //   },
-  //   {
-  //     name: 'twitter:description',
-  //     content:
-  //       "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
-  //   },
-  //   {
-  //     name: 'twitter:image',
-  //     content: import.meta.env.PUBLIC_BLOG_URL + '/about/cover.webp',
-  //   },
-  //   {
-  //     name: 'twitter:image:alt',
-  //     content: 'Image for about page',
-  //   },
-  // ],
+  links: [
+    {
+      rel: 'preload',
+      // @ts-expect-error fetchpriority is not supported by all browsers
+      fetchpriority: 'high',
+      as: 'image',
+      type: 'image/webp',
+      href: '/about/cover.webp',
+    },
+  ],
+  meta: [
+    {
+      name: 'description',
+      content:
+        "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
+    },
+    {
+      property: 'og:title',
+      content: 'About Me | LeoNerd',
+    },
+    {
+      property: 'og:description',
+      content:
+        "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
+    },
+    {
+      property: 'og:image',
+      content: import.meta.env.PUBLIC_BLOG_URL + '/about/cover.webp',
+    },
+    {
+      property: 'og:image:alt',
+      content: 'Image for about page',
+    },
+    {
+      property: 'og:image:width',
+      content: '1000',
+    },
+    {
+      name: 'og:image:height',
+      content: '600',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:site',
+      content: '@WeiTangLin1',
+    },
+    {
+      name: 'twitter:title',
+      content: 'About Me | LeoNerd',
+    },
+    {
+      name: 'twitter:description',
+      content:
+        "Explore LeoNerd's World, a captivating blog where technology, personal growth, and creativity converge.",
+    },
+    {
+      name: 'twitter:image',
+      content: import.meta.env.PUBLIC_BLOG_URL + '/about/cover.webp',
+    },
+    {
+      name: 'twitter:image:alt',
+      content: 'Image for about page',
+    },
+  ],
 };
